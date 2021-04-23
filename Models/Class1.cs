6294a02b30c8b039656cb852b6095549d1450090
hamizou11 <hamza.marwani@esprit.tn>
2021-04-23 23:59:42 +0100

    1st

---
+++ Models/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DariGroupe.Models
+{
+    public class Class1
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string date { get; set; }
+        public string content { get; set; }
+        public int likes { get; set; }
+        public int dislikes { get; set; }
+
+        public ArrayList databaseFile = new ArrayList();
+        public ArrayList comments = new ArrayList();
+
+      
+    }
+}
