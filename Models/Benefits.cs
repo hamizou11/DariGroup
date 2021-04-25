a6fc1834fe79eab720ef4e4f149285725f642554
AmirBH0 <77807736+AmirBH0@users.noreply.github.com>
2021-04-25 20:28:04 +0100

    my first commit

---
+++ Models/Benefits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DariGroupe.Models
+{
+    public class Benefits
+    {
+        public string cam { get; set; }
+
+        public string ins { get; set; }
+    }
+}
