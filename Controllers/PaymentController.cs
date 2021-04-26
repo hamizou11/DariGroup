e163707bda87424500e026669fe2ae1d7e0ef3f4
AmirBH0 <77807736+AmirBH0@users.noreply.github.com>
2021-04-26 18:12:00 +0100

    add Paypal Payment Service Consumption and its views

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using DariGroupe.Models;
+
+namespace DariGroupe.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET: Payment
+        public ActionResult Index()
+        {
+            Paylink paylink = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:8090/api/");
+                var responseTask = client.GetAsync("paypal/pay/1");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readJob = result.Content.ReadAsAsync<Paylink>();
+                    readJob.Wait();
+                    paylink = readJob.Result;
+                }
+                else
+                {
+                    paylink = null;
+                    ModelState.AddModelError(string.Empty, "Server Error!");
+                }
+            }
+
+            return View(paylink);
+        }
+    }
+}
---
+++ Models/Paylink.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DariGroupe.Models
+{
+    public class Paylink
+    {
+        public string linktext { get; set; }
+    }
+}
