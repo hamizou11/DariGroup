2d5ab2db1db9cf0eb726291a61bbac9c9a143ad2
AmirBH0 <77807736+AmirBH0@users.noreply.github.com>
2021-04-26 12:47:46 +0100

    Add Consuming Benefits Service And Creating Its View

---
+++ Controllers/BenefitsController.cs
@@ -0,0 +1,44 @@
+﻿using DariGroupe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DariGroupe.Controllers
+{
+    public class BenefitsController : Controller
+    {
+        // GET: Benefits
+        public ActionResult Index()
+        {
+            //IEnumerable<Benefits> benefits = null;
+            Benefits benefits = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:8090/api/");
+                var responseTask = client.GetAsync("subscription/checkBenefits/1");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    //var readJob = result.Content.ReadAsAsync<IList<Benefits>>();
+                    var readJob = result.Content.ReadAsAsync<Benefits>();
+                    readJob.Wait();
+                    benefits = readJob.Result;
+                }
+                else
+                {
+                    //Return Error Code Here
+                    //benefits = Enumerable.Empty<Benefits>();
+                    benefits = null;
+                    ModelState.AddModelError(string.Empty, "Server Error!");
+                }
+            }
+
+            return View(benefits);
+        }
+    }
+}
