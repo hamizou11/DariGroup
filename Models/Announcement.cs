816fd5e96b5b15dfa8360c079ca43368433b77d0
hediane <49047327+hediane@users.noreply.github.com>
2021-04-26 19:57:12 +0200

    first commit

---
+++ Models/Announcement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DariGroupe.Models
+{
+    public class Announcement
+    {
+
+		public long id { get; set; }
+		public String title { get; set; }
+		public String description { get; set; }
+		public Boolean available { get; set; }
+		public String type { get; set; }
+		public DateTime startDate { get; set; }
+		public DateTime endDate { get; set; }
+		public long duration { get; set; }
+	}
+}
