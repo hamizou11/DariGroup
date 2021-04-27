22aae64da8b4542924471a5f62a05247beb51838
hamizou11 <hamza.marwani@esprit.tn>
2021-04-27 02:32:58 +0100

    2nd

---
+++ Controllers/RentingannonceController.cs
@@ -0,0 +1,98 @@
+﻿using DariGroupe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+using System.Web;
+using System.Web.Mvc;
+
+namespace DariGroupe.Controllers
+{
+    public class RentingannonceController : Controller
+    {
+        // GET: Rentingannonce
+        public ActionResult Renting(string Keyword)
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("https://localhost:44392");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("http://localhost:8084/SpringMVC/servlet/renting/retrieves-all-rentingannonce").Result;
+            /* HttpResponseMessage responses = Client.GetAsync("http:localhost:8084/SpringMVC/servlet/renting/actualite").Result;*/
+            HttpResponseMessage responsesearch = Client.GetAsync("http://localhost:8084/SpringMVC/servlet/renting/search/" + Keyword + "").Result;
+            if (Keyword == "all")
+            {
+                ViewBag.resultse = response.Content.ReadAsAsync<List<Rentingannonce>>().Result;
+            }
+            else
+            {
+                // ViewBag.resultsea = responsesearch.Content.ReadAsAsync<List<Rentingannonce>>().Result;
+            }
+
+
+            ViewBag.Message = "Your application description page.";
+
+            return View("~/Views/RentingAnnonce/Renting.cshtml");
+        }
+        public ActionResult Index()
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("https://localhost:44392/");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("http://localhost:8084/SpringMVC/servlet/renting/retrieves-all-rentingannonce").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.result = response.Content.ReadAsAsync<IEnumerable<Rentingannonce>>().Result;
+            }
+            else
+            {
+                ViewBag.result = " erreur";
+            }
+
+
+            return View("~/Views/RentingAnnonce/Renting.cshtml");
+        }
+
+
+
+        public ActionResult LastRenting()
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("https://localhost:44392/");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("http://localhost:8084/SpringMVC/servlet/renting/latestRentingAnnonce").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.result = response.Content.ReadAsAsync<IEnumerable<Rentingannonce>>().Result;
+            }
+            else
+            {
+                ViewBag.result = " erreur";
+            }
+
+
+            return View("~/Views/RentingAnnonce/Renting.cshtml");
+        }
+        public ActionResult Best()
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("https://localhost:44392/");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("http://localhost:8084/SpringMVC/servlet/renting/BestReviewed").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.result = response.Content.ReadAsAsync<IEnumerable<Rentingannonce>>().Result;
+            }
+            else
+            {
+                ViewBag.result = " erreur";
+            }
+
+
+            return View("~/Views/RentingAnnonce/Renting.cshtml");
+        }
+    }
+
+}
---
+++ Models/Rentingannonce.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DariGroupe.Models
+{
+    public class Rentingannonce
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string adresse { get; set; }
+        public string video { get; set; }
+        public double price { get; set; }
+        public double innerSurface { get; set; }
+        public double planeSurface { get; set; }
+        public int roomNumber { get; set; }
+        public double statePrice { get; set; }
+        public string photoIdentity { get; set; }
+        public string engagementLettre { get; set; }
+        public bool favoriteAnnonce { get; set; }
+        public DateTime dateDebut { get; set; }
+        public DateTime dateFin { get; set; }
+        public int nbrPersonne { get; set; }
+        public bool rented { get; set; }
+        public DateTime createdAt { get; set; }
+        public string adState { get; set; }
+    }
+}
+    
